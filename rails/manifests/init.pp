ba86d7fa4fff4b5d2ca8fc1ed7887a22509af761
Teng <otec.teng@gmail.com>
2014-12-03 16:06:40 +0800

    Update init.pp

--- rails/manifests/init.pp
+++ rails/manifests/init.pp
@@ -1,16 +1,26 @@
-class rails {
+class china{
   file { "/root/gem_tb.sh":
     owner => "root",
     mode  => "775",
     content => "gem sources -r http://rubygems.org/
 gem sources -a http://ruby.taobao.org/",
   }->
-  exec{ "add gem":
+  exec{ "change gem":
     command => "/bin/bash /root/gem_tb.sh",
-  }->
+  }
+}
+class rails($china=false) {
   package{["mongodb","nodejs","nginx","redis-server","make","ruby-dev"]:
     ensure=>"installed",
   }->
+  file{ "/etc/nginx/nginx.conf":
+    owner => "root",
+    mode  => "775",
+    source => "puppet:///modules/rails/nginx.conf",
+  }
+  if($china){
+    require china
+  }
   package{["rails"]:
     ensure=>"3.2.19",
     provider=>"gem"
@@ -18,10 +28,6 @@ gem sources -a http://ruby.taobao.org/",
   package{["sidekiq","unicorn"]:
     ensure=>"installed",
     provider=>"gem"
-  }->
-  file { "/etc/nginx/nginx.conf":
-    owner => "root",
-    mode  => "775",
-    source => "puppet:///modules/rails/nginx.conf",
   }
 }
+
